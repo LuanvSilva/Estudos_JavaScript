dd36362a7c16087f90082365c98c30a9513439b6
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-02-02 18:17:02 -0300

    Atualiza

--- classe-e-POO-2.js
+++ classe-e-POO-2.js
@@ -1,46 +1,83 @@
 class Spaceship {
-    static get DESACELARATION() {
-        return 0.17
+    static get DECELERATION() {
+        return 0.17;
     }
-    constructor(name,maxQuantity){
-        this.name = name
-        this.maxQuantity = maxQuantity
-        this.currentVelocity = 0
-
+    constructor(name, maxQuantity) {
+        this.name = name;
+        this.maxQuantity = maxQuantity;
+        this.currentVelocity = 0;
     }
-    speedUp(acceleration){
-        this.currentVelocity += acceleration * (1 - Spaceship.DESACELARATION)
+    speedUp(acceleration) {
+        this.currentVelocity += acceleration * (1 - Spaceship.DECELERATION);
     }
 }
 
 class TransportSpaceship extends Spaceship {
-    constructor(name,crewQuantity,sitsQuantity){
-        this.sitsQuantity = sitsQuantity
+    constructor(name, crewQuantity, sitsQuantity) {
+        super(name, crewQuantity);
+        this.sitsQuantity = sitsQuantity;
     }
 }
 
-class app{
-    constructor(){
-        this.Spaceship = null
+class App {
+    constructor() {
+        this.spaceship = null;
+    }
+    start() {
+        let chosenOption;
+        do {
+            chosenOption = this.showInitialMenu();
+            this.redirectFeature(chosenOption);
+        } while (chosenOption != '3');
+        this.printAndExit();
     }
     enrollSpaceship() {
-        let spaceshipName = prompt("Qual o nome da nave ?")
-        let crewQuantity = prompt("Qual a quantidade de tripulantes?")
-        let spaceshipKind = this.askForSpaceshipKind()
-        if(spaceshipKind =='1'){
-            let weaponsQuantity = prompt("Quantas armas a nave possui")
-            this.spaceship = new BattleSpaceship(spaceshipName,crewQuantity,weaponsQuantity)
-        } else{
-            let sitsQuantity = prompt('Quantos lugares a nave possui')
-            this.spaceship = new TransportSpaceship(spaceshipName,crewQuantity,sitsQuantity)
+        let spaceshipName = prompt("Qual o nome da nave?");
+        let crewQuantity = prompt("Qual a quantidade de tripulantes?");
+        let spaceshipKind = this.askForSpaceshipKind();
+        if (spaceshipKind == '1') {
+            let weaponsQuantity = prompt("Quantas armas a nave possui");
+            this.spaceship = new BattleSpaceship(spaceshipName, crewQuantity, weaponsQuantity);
+        } else {
+            let sitsQuantity = prompt("Quantos lugares a nave possui");
+            this.spaceship = new TransportSpaceship(spaceshipName, crewQuantity, sitsQuantity);
         }
     }
     askForSpaceshipKind() {
-        let chosenOption 
-        while('1','2'.includes(chosenOption)){
-            chosenOption = prompt('Qual o tipo da nave \1 - Batalha \n2- Transporte')
+        let chosenOption;
+        while (!['1', '2'].includes(chosenOption)) {
+            chosenOption = prompt("Qual o tipo da nave\n1 - Batalha \n2 - Transporte");
         }
-        return chosenOption
+        return chosenOption;
     }
-
-}  
+    showInitialMenu() {
+        const promptMessage = 'O que você deseja fazer?\n' +
+            '1- Acelerar a nave\n' +
+            '2- Trocar a nave\n' +
+            '3- Imprimir a nave';
+        let chosenOption = prompt(promptMessage);
+        while (!['1', '2', '3'].includes(chosenOption)) {
+            chosenOption = prompt(promptMessage);
+        }
+        return chosenOption;
+    }
+    redirectFeature(chosenOption) {
+        switch (chosenOption) {
+            case '1':
+                this.accelerateSpaceship();
+                break;
+            case '2':
+                this.enrollSpaceship();
+                break;
+        }
+    }
+    accelerateSpaceship() {
+        let acceleration = Number(prompt("Quanto gostaria de acelerar?"));
+        this.spaceship.speedUp(acceleration);
+    }
+    printAndExit() {     
+           let finalMessage = 'Nome: ' + this.spaceship.name + '\n'
+    }
+}
+const app1 = new App
+app1.start()
